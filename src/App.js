f7d24469ebc8bb1bf83794107ea2e6832ced0440
Miguel Gomez <andresmgomez23@gmail.com>
2022-06-15 21:16:48 -0400

    Add breadcrumb and page to display a list of recipes dynamically

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AddRecipePage } from './pages/AddRecipe';
-import { DetailRecipePage } from './pages/DetailRecipe';
+import { DisplaRecipesPage } from './pages/DisplayRecipes';
+// import { DetailRecipePage } from './pages/DetailRecipe';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<DetailRecipePage />} />
+					<Route path='/' element={<DisplaRecipesPage />} />
 					<Route path='/add-recipe' element={<AddRecipePage />} />
 				</Routes>
 			</BrowserRouter>
---
+++ src/pages/DisplayRecipes.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BreadcrumbComponent } from '../components/common/Breadcrumb/Breadcrumb';
+import { RecipeCardComponent } from '../components/common/Card/RecipeCard';
+
+export const DisplaRecipesPage = () => {
+	const [recipes, setRecipes] = useState();
+
+	useEffect(() => {
+		fetch('https://final-project-api-hostin-13b05.web.app/recipes')
+			.then(res => res.json())
+			.then(data => setRecipes(data))
+			.catch(err => console.error(err));
+	}, []);
+
+	return (
+		<>
+			<BreadcrumbComponent title='List of Recipes' task='Display Recipes' />
+			<section>
+				<Container>
+					<Row>
+						<Col>
+							{!recipes ? (
+								<h3 className='text-center mt-3'>Please Wait...</h3>
+							) : (
+								recipes?.map(recipe => {
+									return (
+										<RecipeCardComponent recipe={recipe} key={recipe.id} />
+									);
+								})
+							)}
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</>
+	);
+};
